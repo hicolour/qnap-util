9310b155b82c9af84a7b34cfa04d9c7995ffb2de
Marek Prochera <marek.prochera@gmail.com>
2013-03-10 10:49:33 +0100

    find

--- crontab-jobs/qnap-crontab-napi.sh
+++ crontab-jobs/qnap-crontab-napi.sh
@@ -3,19 +3,19 @@
 movies="/share/Movies/movies-new"
 
 FILE_TYPES="-name \"*.mkv\" -o -name \"*.avi\" "
-find -L . \( ${FILE_TYPES} \)
 
 
 
 
 search(){
   cd $movies
-  for file in $(find -L . \( ${FILE_TYPES} \))
+  echo Find in $(pwd)
+  for file in $(/opt/bin/find -L . \( ${FILE_TYPES} \))
   do
 
     echo File: $file
 
   done
 }
-
-
+echo init
+search
